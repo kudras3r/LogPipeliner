3ce1d215cf4b33097d289dc5f003fcc20dddbb25
kudras3r <d8880888@mail.ru>
2024-11-09 19:31:01 +0300

    TODO update

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ## Roadmap
 
 - 07/11/24 | Registration on https://tenzir.com/ ; Read the introdution docs.
+- 09/11/24 | Install tenzir ; First test log sending.
 
 
 
--- cmd/pipeliner/main.go
+++ cmd/pipeliner/main.go
@@ -2,4 +2,8 @@ package main
 
 func main() {
 	// TODO ->
+
+	// init CLI (only requests the path to the dir with logs)
+	// init validator (check .log extension)
+	// init sender (send logs to api)
 }
