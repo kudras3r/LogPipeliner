296fb96c77408f8494373e41d5737743533b4eb0
kudras3r <d8880888@mail.ru>
2024-11-14 02:17:20 +0300

    parser update

--- cmd/pipeliner/main.go
+++ cmd/pipeliner/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"github.com/kudras3r/LogPipeliner/internal/logs/collect"
+	"fmt"
+
+	"github.com/kudras3r/LogPipeliner/internal/logs/parse"
+)
+
+const (
+	logsDir  = "/home/kud/Code/go/src/LogPipeliner/test_logs"
+	logsType = "dpkg"
 )
 
 func main() {
 	// TODO ->
 
 	// init parser
+	logs, err := parse.Dpkg(logsDir)
+	if err != nil {
+		fmt.Printf("< ERROR > error when reading the dir : %s\n", err)
+	}
+	for _, l := range logs {
+		fmt.Println(l.Timestamp, l.Content)
+	}
+
 	// init sender
 
 	// init collector
-	collect.RunCollector()
+	// collect.RunCollector()
 
 }
--- internal/logs/parse/parse.go
+++ internal/logs/parse/parse.go
@@ -1,14 +1,68 @@
 package parse
 
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 // Walk around logs dir and parse logs in structs.
 
-// in future we can do composition to describe specific logs
-// like AuthLog, BootLog...
-type Log struct {
+type BaseLog struct {
 	Timestamp string `json:"timestamp"`
 	Content   string `json:"content"`
 }
 
-func Parse(logsType string) {
+type DpkgLog struct {
+	BaseLog
+}
 
+func Dpkg(path string) ([]DpkgLog, error) {
+	var result []DpkgLog
+	var timestamp, content string
+
+	err := filepath.Walk(path, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			filename := info.Name()
+			if strings.Contains(filename, ".log") && strings.HasPrefix(filename, "dpkg") {
+				file, err := os.Open(filePath)
+				if err != nil {
+
+				}
+				defer file.Close()
+
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					lineParts := strings.Split(scanner.Text(), " ")
+
+					if len(lineParts) < 3 {
+						break
+					}
+
+					timestamp = lineParts[0] + " " + lineParts[1]
+					content = strings.Join(lineParts[2:], " ")
+
+					log := DpkgLog{
+						BaseLog: BaseLog{
+							Timestamp: timestamp,
+							Content:   content,
+						},
+					}
+					result = append(result, log)
+				}
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
 }
